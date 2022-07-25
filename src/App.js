7f0b7ceed1c09b7ad80a27e22937358cf2b0bccc
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2022-07-25 14:09:39 +0300

    Included a link to GitHub repository

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, React!</h1>
+        <h1>Weather App in React</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <Weather/>
+        <Weather />
       </header>
+      <footer>
+        <a
+          href="https://github.com/ok-sana-code/react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open-source code
+        </a>
+        , by Oksana Shvets from{" "}
+        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+          She Codes
+        </a>
+      </footer>
     </div>
   );
 }
